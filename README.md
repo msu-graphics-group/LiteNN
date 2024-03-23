a98a00113b6aabb7073a42ee0e939a3c1b1fb482
Sammael <Sammael>
2024-03-23 09:55:21 +0300

    updated slicer_execute script and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NeuralCore
+
+## How to build (standalone)
+1) download and build kernel slicer
+2) bash slicer_execute.sh absolute/path/to/slicer/folder absolute/path/to/slicer/executable
+3) cmake CMakeLists.txt -DMODULE_VULKAN=ON -DSLICER_DIR=/absolute/path/to/slicer/folder
+4) make -j8
+
+## Run
+
+./nn_test - it will run perform_tests function with a bunch of different tests
--- slicer_execute.sh
+++ slicer_execute.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd ../../../kernel_slicer
-./kslicer ../grade/modules/neuralCore/tensor_processor_impl.cpp -mainClass TensorProcessorImpl -stdlibfolder $PWD/TINYSTL -pattern ipv -reorderLoops YX -I$PWD/apps/LiteMath ignore -I$PWD/apps/LiteMathAux ignore -I$PWD/TINYSTL ignore -shaderCC glsl -suffix _GPU -DKERNEL_SLICER -v 
-cd ../grade/modules/neuralCore
+# slicer_execute absolute/path/to/slicer/folder absolute/path/to/slicer/executable
+start_dir=$PWD
+cd $1
+$2 $start_dir/tensor_processor_impl.cpp -mainClass TensorProcessorImpl -stdlibfolder $PWD/TINYSTL -pattern ipv -reorderLoops YX -I$PWD/apps/LiteMath ignore -I$PWD/apps/LiteMathAux ignore -I$PWD/TINYSTL ignore -shaderCC glsl -suffix _GPU -DKERNEL_SLICER -v 
+cd $start_dir
 rm -r ../../shaders_gpu
 mkdir ../../shaders_gpu
 cd shaders_gpu
