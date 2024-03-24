5dd3276d3e5023a6167babdf07142773fee3193a
Sammael <Sammael>
2024-03-24 15:33:38 +0300

    check average and max error for matrix multiplication benchmark. Error is calculated in relation to calculation all values as long doubles

--- nn_tests_performance.cpp
+++ nn_tests_performance.cpp
@@ -28,18 +28,36 @@ namespace nn
   {
     printf("TEST 1. MATRIX MULTIPLICATION BENCHMARK\n");
 
-    std::vector<unsigned> sizes = {1, 4, 16, 64, 256, 1024, 4096};
+    std::vector<unsigned> sizes = {1, 16, 64, 256, 1024, 2048};
 
     std::vector<float> A(sizes.back()*sizes.back(), 0);
     std::vector<float> B(sizes.back()*sizes.back(), 0);
     std::vector<float> C(sizes.back()*sizes.back(), 0);
+    std::vector<float> C_ref(sizes.back()*sizes.back(), 0);
     TensorCompiler tc;
 
     for (unsigned size_A : sizes)
     {
       for (unsigned size_B : sizes)
       {
-        unsigned n_tries = size_A == 4096 ? 10 : (size_A == 1024 ? 50 : 500);
+        unsigned n_tries = size_A == 2048 ? 25 : (size_A == 1024 ? 50 : 500);
+
+        for (int i=0;i<size_A*size_A;i++)
+          A[i] = 2.0*rand()/RAND_MAX + 1.0;
+
+        for (int i=0;i<size_A*size_B;i++)
+          B[i] = 2.0*rand()/RAND_MAX + 1.0;
+        
+        for (int i=0;i<size_B;i++)
+        {
+          for (int j=0;j<size_A;j++)
+          {
+            long double val = 0;
+            for (int k=0;k<size_A;k++)
+              val += (long double)A[j*size_A + k] * (long double)B[i*size_A + k];
+            C_ref[j*size_B + i] = val;
+          }
+        }
 
     auto t_1 = std::chrono::steady_clock::now();
         tc.start_program();
@@ -70,14 +88,36 @@ namespace nn
         TensorProcessor::get_output("C", C.data(), C.size());
     
     auto t_5 = std::chrono::steady_clock::now();
+
+        long double sum_diff = 0;
+        double max_diff = 0;
+        unsigned max_diff_i=0, max_diff_j=0;
+
+        for (int i=0;i<size_A;i++)
+        {
+          for (int j=0;j<size_B;j++)
+          {
+            //printf("(%u %u) %f %f\n",i,j, C_ref[i*size_B + j], C[i*size_B + j]);
+            double diff = abs(C_ref[i*size_B + j] - C[i*size_B + j]);
+            sum_diff += diff;
+            if (diff > max_diff)
+            {
+              max_diff = diff;
+              max_diff_i = i;
+              max_diff_j = j;
+            }
+          }
+        }
+
         
         float t_compile = 0.001/n_tries*std::chrono::duration_cast<std::chrono::microseconds>(t_2 - t_1).count();
         float t_input =   0.001/n_tries*std::chrono::duration_cast<std::chrono::microseconds>(t_3 - t_2).count();
         float t_execute = 0.001/n_tries*std::chrono::duration_cast<std::chrono::microseconds>(t_4 - t_3).count();
         float t_output =  0.001/n_tries*std::chrono::duration_cast<std::chrono::microseconds>(t_5 - t_4).count();
 
-        printf("matmul (%4ux%4u)*(%4ux%4u): exec %7.2f ms, overhead %5.2f+%5.2f+%5.2f ms\n", size_A, size_A, size_B, size_A, 
-               t_execute, t_compile, t_input, t_output);
+        printf("matmul (%4ux%4u)*(%4ux%4u): exec %7.2f ms, overhead %5.2f+%5.2f+%5.2f ms, av. diff %.2Le, max diff %.2le at (%u %u)\n", 
+               size_A, size_A, size_B, size_A, t_execute, t_compile, t_input, t_output,
+               sum_diff/(size_A*size_B), max_diff, max_diff_i, max_diff_j);
       }      
     }
   }
