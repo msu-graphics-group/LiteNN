fb826727257c4bde67a6caf61c5bf5fbb7dcbb9e
Sammael <Sammael>
2024-03-29 10:13:40 +0300

    fixed slicer_execute.sh script

--- slicer_execute.sh
+++ slicer_execute.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 # slicer_execute absolute/path/to/slicer/folder absolute/path/to/slicer/executable
+#e.g. bash slicer_execute.sh ~/kernel_slicer/ ~/kernel_slicer/kslicer
 start_dir=$PWD
 cd $1
-$2 $start_dir/tensor_processor_impl.cpp -mainClass TensorProcessorImpl -stdlibfolder $PWD/TINYSTL -pattern ipv -reorderLoops YX -I$PWD/apps/LiteMath ignore -I$PWD/apps/LiteMathAux ignore -I$PWD/TINYSTL ignore -shaderCC glsl -suffix _GPU -DKERNEL_SLICER -v 
+
+$2 $start_dir/tensor_processor_impl.cpp \
+-mainClass TensorProcessorImpl \
+-stdlibfolder $PWD/TINYSTL \
+-pattern ipv \
+-reorderLoops YX \
+-I$PWD/apps/LiteMath ignore \
+-I$PWD/apps/LiteMathAux ignore \
+-I$PWD/TINYSTL ignore \
+-shaderCC glsl \
+-suffix _GPU \
+-DKERNEL_SLICER -v 
+
 cd $start_dir
-rm -r ../../shaders_gpu
-mkdir ../../shaders_gpu
 cd shaders_gpu
 bash build.sh
-find -name "*.spv" | xargs cp --parents -t ../../../shaders_gpu
 cd ..
